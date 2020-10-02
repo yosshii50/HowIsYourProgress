ffe6d82cda86bb8c783bab319848b1245e874275
yosshii50 <24950665+yosshii50@users.noreply.github.com>
2020-10-02 22:01:31 +0900

    Create index.php

---
+++ index.php
@@ -0,0 +1,11 @@
+<html>
+<meta http-equiv="content-type" charset="utf-8">
+<head>
+<title>進捗どうですか？</title>
+</head>
+<body>
+<script language="JavaScript">
+document.writeln("進捗どうですか？<BR>");
+</script>
+</body>
+</html>
