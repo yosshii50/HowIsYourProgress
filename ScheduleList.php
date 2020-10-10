f56853abad1a9763af04f33a65c4ff934876ca94
“HomePC-WSL-Ubuntu” <kasaubunts@gmail.com>
2020-10-10 22:17:59 +0900

    Update

--- ScheduleList.php
+++ ScheduleList.php
@@ -3,11 +3,19 @@
 // ファイルから読み込み、行ごとの配列に格納
 $Filedata_arr = file( 'Schedule.txt' );
 
+// 行番号
+$LineNo = 0;
+
 // 行ごとの配列から、行ごとに処理
 foreach ( $Filedata_arr as &$Line_data ){
 	
+	$Line_arr = [];
+	
+	$LineNo += 1;
+	$Line_arr[] = $LineNo;
+	
 	// [TAB]区切りを配列に格納
-	$Line_arr = explode( "\t" , $Line_data );
+	$Line_arr += explode( "\t" , $Line_data );
 	//                  ↑仕様上の制限から[']はNG
 	
 	// 返信用配列に配列ごと保存
