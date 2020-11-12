4edc7d735aa35e4982a6a975ba296eba73da34b0
yosshii50 <kasaubunts@gmail.com>
2020-11-12 10:41:00 +0900

    西暦設定が１年ズレている問題の修正／和暦コンボボックスの初期値設定

--- GengouHenkan.js
+++ GengouHenkan.js
@@ -2,7 +2,7 @@
 // 元号変換
 
 // 表示に必要なHTML出力
-document.write('<select id="ComboBox1" onchange="ComboBoxChange1();"/>');
+document.write('<select id="GengouHenkanComboBox" onchange="ComboBoxChange1();"/>');
 document.write('<input type="text" id="Text1"size="5" onkeyup="TextChange1();"> ');
 document.write('<span id="ViewString"></span>');
 
@@ -21,7 +21,7 @@ document.addEventListener('DOMContentLoaded', function(){
 // 内容初期化
 function ComboBoxInit1() {
 	
-	var TmpCmbBox = document.getElementById("ComboBox1");
+	var TmpCmbBox = document.getElementById("GengouHenkanComboBox");
 	
 	// 固定値セット
 	AddComboBoxItemStr( TmpCmbBox , 'H30' );
@@ -32,25 +32,50 @@ function ComboBoxInit1() {
 	AddComboBoxItemStr( TmpCmbBox , 'R3'  );
 	
 	// 変動値セット
+	var BaseNen = 2019; // 現元号の基準年
 	var ima = new Date(); // 今日の日付
-	var YearST = ima.getFullYear() - 2020; // ２年前の令和年取得
+	var YearST = ima.getFullYear() - BaseNen - 1; // ２年前の令和年取得
 	var YearED = YearST + 5; // ５年分
 	
 	for( var YearWK = YearST ; YearWK < YearED ; YearWK++ ) {
+		
 		// 令和3年までは初期値で入れているので、令和3年以降のみ追加
 		if( YearWK > 3 ) {
-			AddComboBoxItemStr( TmpCmbBox , 'R' + YearWK  );
+			AddComboBoxItemStr( TmpCmbBox , 'R' + YearWK );
+		}
+		
+		// 今年を初期値に設定
+		if( YearWK == ima.getFullYear() - BaseNen + 1 ) {
+			
+			var SetIdx = GetComboBoxValue2Index( TmpCmbBox , 'R' + YearWK );
+			TmpCmbBox.options[SetIdx].selected = true
+			
 		}
 	}
 	
 }
 
+// コンボボックスの value から index 番号を取得
+function GetComboBoxValue2Index( SetCmbBox , ItemValue ) {
+	for( var WrkIdx = 0 ; WrkIdx < SetCmbBox.options.length ; WrkIdx++ ) {
+		if( SetCmbBox.options[ WrkIdx ].value === ItemValue ) {
+			return WrkIdx;
+		}
+	}
+	return -1;
+}
+
 // コンボボックスに項目追加
-function AddComboBoxItemStr( SetCmbBox , SetItemStr ) {
+function AddComboBoxItemStr( SetCmbBox , SetItemStr , IsSelected ) {
 	
 	var TmpOption = document.createElement("option");
-	TmpOption.text = SetItemStr;
+	TmpOption.text  = SetItemStr;
+	TmpOption.value = SetItemStr;
 	SetCmbBox.appendChild( TmpOption );
+	
+	if( IsSelected == true ) {
+		TmpOption.selected = true;
+	}
 	
 }
 
@@ -58,7 +83,7 @@ function AddComboBoxItemStr( SetCmbBox , SetItemStr ) {
 function ComboBoxChange1() {
 	
 	// 変更値取得
-	var WrkString = document.getElementById("ComboBox1").value;
+	var WrkString = document.getElementById("GengouHenkanComboBox").value;
 	
 	// テキストボックスに上書き
 	document.getElementById("Text1").value = WrkString;
@@ -71,11 +96,11 @@ function ComboBoxChange1() {
 // 文字内容変更時
 function TextChange1() {
 	
-//	var WarekiList = [ [ 'M' , 1867 , '明治' ]
-//	                 , [ 'T' , 1911 , '大正' ]
-//	                 , [ 'S' , 1925 , '昭和' ]
-	var WarekiList = [ [ 'H' , 1988 , '平成' ]
-	                 , [ 'R' , 2018 , '令和' ]
+//	var WarekiList = [ [ 'M' , 1868 , '明治' ]
+//	                 , [ 'T' , 1912 , '大正' ]
+//	                 , [ 'S' , 1926 , '昭和' ]
+	var WarekiList = [ [ 'H' , 1989 , '平成' ]
+	                 , [ 'R' , 2019 , '令和' ]
 	                 ];
 	
 	// 文字取得
@@ -87,7 +112,7 @@ function TextChange1() {
 	for( WrkIdx = 0 ; WrkIdx < WarekiList.length  ; WrkIdx++ ) {
 		// 先頭文字から判別
 		if( WrkStr.slice( 0 , 1 ).toUpperCase() == WarekiList[ WrkIdx ][0] ){
-			WrkNen = Number( WrkStr.slice( 1 ) ) + WarekiList[ WrkIdx ][1];
+			WrkNen = Number( WrkStr.slice( 1 ) ) + WarekiList[ WrkIdx ][1] - 1;
 			break;
 		}
 	}
@@ -100,8 +125,8 @@ function TextChange1() {
 	// 和暦ごとの表示内容生成
 	var SetStr = '';
 	for( WrkIdx = 0 ; WrkIdx < WarekiList.length  ; WrkIdx++ ) {
-		if( WrkNen >  WarekiList[ WrkIdx ][1] ) {
-			SetStr =  WarekiList[ WrkIdx ][2] + ( WrkNen -  WarekiList[ WrkIdx ][1] ) + '年 ' + SetStr;
+		if( WrkNen >=  WarekiList[ WrkIdx ][1] ) {
+			SetStr =  WarekiList[ WrkIdx ][2] + ( WrkNen -  WarekiList[ WrkIdx ][1] + 1 ) + '年 ' + SetStr;
 		}
 	}
 	
